2bf4ffebda7d681e43987adf4518d0414d1462bf
Samuel Buteau <Samuel.Buteau@dal.ca>
2019-07-02 14:07:20 -0300

    fixed configuration bug and added script for release

---
+++ basic_test_script.bat
@@ -0,0 +1,1 @@
+python manage.py eis_main --mode=import_process_output --logdir=OnePercentTraining --input_dir=import_test --dataset=USER000 --output_dir=OutputData_test
--- release_script.bat
+++ release_script.bat
@@ -1,3 +1,3 @@
-rm db.sqlite3
+del db.sqlite3
 python manage.py migrate
 python manage.py edit_database --mode=add_default_inverse_models
