c54284658f2d637e922999aa1d8992ea5259a9d0
Samuel Buteau <Samuel.Buteau@dal.ca>
2019-07-01 21:00:15 -0300

    fixed errors

--- EISFittingModelDefinitions.py
+++ EISFittingModelDefinitions.py
@@ -137,128 +137,128 @@ def ImpedanceModel(params_, frequencies_, batch_size, model_meta=None):
     :param frequencies:
     :return:
     '''
+    with tf.device('/CPU:0'):
+
+        model_meta = tf.to_double(model_meta)
+
+        params = tf.to_double(params_)
+        frequencies = tf.to_double(frequencies_)
+
+        params_reshaped = tf.expand_dims(params, axis=2)
+
+        batch_zeros = tf.zeros([batch_size, 1], dtype=tf.float64)
+        full_zeros = tf.zeros_like(frequencies, dtype=tf.float64)
 
 
-    model_meta = tf.to_double(model_meta)
 
-    params = tf.to_double(params_)
-    frequencies = tf.to_double(frequencies_)
+        params_to_exp = tf.exp(params_reshaped[:, :INDEX_PHI_WARBURG])
+        params_to_sigm = tf.sigmoid(params_reshaped[:, INDEX_PHI_WARBURG:INDEX_PHI_INDUCTANCE])
+        params_to_neg_sigm = -1. / (1. + tf.square(params_reshaped[:, INDEX_PHI_INDUCTANCE:]))
 
-    params_reshaped = tf.expand_dims(params, axis=2)
+        processed_params = tf.concat([params_to_exp, params_to_sigm, params_to_neg_sigm], axis=1)
 
-    batch_zeros = tf.zeros([batch_size, 1], dtype=tf.float64)
-    full_zeros = tf.zeros_like(frequencies, dtype=tf.float64)
+        '''
+           - Resistor, parameters {R}: Z(W) = R + i * 0
+           - Constant Phase Element, parameters {q, phi}: Z(W) = exp(q) * W^-Phi * (i)^-Phi 
+           - Zarc, parameters {R, W_c, Phi}: Z(W) = R/(1 + (i W/W_c)^(Phi))
+    
+    
+        '''
+        exp_frequencies = tf.exp(frequencies)
 
+        R = processed_params[:, INDEX_R]
 
+        impedance = tf.complex(full_zeros + R, full_zeros)
 
-    params_to_exp = tf.exp(params_reshaped[:, :INDEX_PHI_WARBURG])
-    params_to_sigm = tf.sigmoid(params_reshaped[:, INDEX_PHI_WARBURG:INDEX_PHI_INDUCTANCE])
-    params_to_neg_sigm = -1. / (1. + tf.square(params_reshaped[:, INDEX_PHI_INDUCTANCE:]))
+        imaginary_unit = tf.to_complex128(tf.complex(0., 1.))
 
-    processed_params = tf.concat([params_to_exp, params_to_sigm, params_to_neg_sigm], axis=1)
+        # warburg
 
-    '''
-       - Resistor, parameters {R}: Z(W) = R + i * 0
-       - Constant Phase Element, parameters {q, phi}: Z(W) = exp(q) * W^-Phi * (i)^-Phi 
-       - Zarc, parameters {R, W_c, Phi}: Z(W) = R/(1 + (i W/W_c)^(Phi))
+        bad_piece = tf.pow(imaginary_unit, tf.complex(-processed_params[:, INDEX_PHI_WARBURG], batch_zeros))
+        real_bad_piece = tf.real(bad_piece)
+        imag_bad_piece = tf.imag(bad_piece)
+        bad_piece2 = processed_params[:, INDEX_Q_WARBURG] * tf.pow(exp_frequencies, -processed_params[:, INDEX_PHI_WARBURG]) *tf.expand_dims(1. - model_meta[:, MODEL_META_WARBURG_INCEPTION], axis=1)
 
+        warburg_impedance = tf.complex(
+            real_bad_piece * bad_piece2, imag_bad_piece * bad_piece2
+        )
 
-    '''
-    exp_frequencies = tf.exp(frequencies)
+        impedance += warburg_impedance
 
-    R = processed_params[:, INDEX_R]
+        # inductance
+        phi = processed_params[:, INDEX_PHI_INDUCTANCE]
+        exp_q = processed_params[:, INDEX_Q_INDUCTANCE] * tf.expand_dims(model_meta[:, MODEL_META_INDUCTANCE],axis=1)
 
-    impedance = tf.complex(full_zeros + R, full_zeros)
+        bad_piece = tf.pow(imaginary_unit, tf.complex(-phi, batch_zeros))
+        real_bad_piece = tf.real(bad_piece)
+        imag_bad_piece = tf.imag(bad_piece)
+        bad_piece2 = exp_q * tf.pow(exp_frequencies, -phi)
 
-    imaginary_unit = tf.to_complex128(tf.complex(0., 1.))
+        impedance +=  tf.complex(
+            real_bad_piece * bad_piece2,
+            imag_bad_piece * bad_piece2
+        )
 
-    # warburg
+        # inductance zarc
+        phi = tf.complex(processed_params[:, INDEX_PHI_ZARC_INDUCTANCE], batch_zeros)
+        w_c = tf.complex(processed_params[:, INDEX_W_C_INDUCTANCE], batch_zeros)
+        r = tf.complex(processed_params[:, INDEX_R_ZARC_INDUCTANCE] * tf.expand_dims(model_meta[:, MODEL_META_ZARC_INDUCTANCE],axis=1), batch_zeros)
+        imag_freq = tf.complex(full_zeros, exp_frequencies)
 
-    bad_piece = tf.pow(imaginary_unit, tf.complex(-processed_params[:, INDEX_PHI_WARBURG], batch_zeros))
-    real_bad_piece = tf.real(bad_piece)
-    imag_bad_piece = tf.imag(bad_piece)
-    bad_piece2 = processed_params[:, INDEX_Q_WARBURG] * tf.pow(exp_frequencies, -processed_params[:, INDEX_PHI_WARBURG]) *tf.expand_dims(1. - model_meta[:, MODEL_META_WARBURG_INCEPTION], axis=1)
+        impedance += r / (1. + tf.pow((imag_freq / w_c), phi))
 
-    warburg_impedance = tf.complex(
-        real_bad_piece * bad_piece2, imag_bad_piece * bad_piece2
-    )
-
-    impedance += warburg_impedance
-
-    # inductance
-    phi = processed_params[:, INDEX_PHI_INDUCTANCE]
-    exp_q = processed_params[:, INDEX_Q_INDUCTANCE] * tf.expand_dims(model_meta[:, MODEL_META_INDUCTANCE],axis=1)
-
-    bad_piece = tf.pow(imaginary_unit, tf.complex(-phi, batch_zeros))
-    real_bad_piece = tf.real(bad_piece)
-    imag_bad_piece = tf.imag(bad_piece)
-    bad_piece2 = exp_q * tf.pow(exp_frequencies, -phi)
-
-    impedance +=  tf.complex(
-        real_bad_piece * bad_piece2,
-        imag_bad_piece * bad_piece2
-    )
-
-    # inductance zarc
-    phi = tf.complex(processed_params[:, INDEX_PHI_ZARC_INDUCTANCE], batch_zeros)
-    w_c = tf.complex(processed_params[:, INDEX_W_C_INDUCTANCE], batch_zeros)
-    r = tf.complex(processed_params[:, INDEX_R_ZARC_INDUCTANCE] * tf.expand_dims(model_meta[:, MODEL_META_ZARC_INDUCTANCE],axis=1), batch_zeros)
-    imag_freq = tf.complex(full_zeros, exp_frequencies)
-
-    impedance += r / (1. + tf.pow((imag_freq / w_c), phi))
-
-    for index in range(NUMBER_OF_ZARC):
-        # zarc
-        phi = tf.complex(processed_params[:, INDEX_PHI_ZARC_OFFSET + index], batch_zeros)
-        w_c = tf.complex(processed_params[:, INDEX_W_C_ZARC_OFFSET + index], batch_zeros)
-        r = tf.complex(processed_params[:, INDEX_R_ZARC_OFFSET + index] * tf.expand_dims(model_meta[:, MODEL_META_ZARC + index],axis=1), batch_zeros)
+        for index in range(NUMBER_OF_ZARC):
+            # zarc
+            phi = tf.complex(processed_params[:, INDEX_PHI_ZARC_OFFSET + index], batch_zeros)
+            w_c = tf.complex(processed_params[:, INDEX_W_C_ZARC_OFFSET + index], batch_zeros)
+            r = tf.complex(processed_params[:, INDEX_R_ZARC_OFFSET + index] * tf.expand_dims(model_meta[:, MODEL_META_ZARC + index],axis=1), batch_zeros)
 
 
-        if index == 0:
-            bad_piece2 = (processed_params[:, INDEX_Q_WARBURG]/processed_params[:, INDEX_R_ZARC_OFFSET + index]) * tf.pow(
-                exp_frequencies,
-                -processed_params[:,INDEX_PHI_WARBURG]
-            ) * tf.expand_dims(
-                model_meta[:, MODEL_META_WARBURG_INCEPTION], axis=1)
+            if index == 0:
+                bad_piece2 = (processed_params[:, INDEX_Q_WARBURG]/processed_params[:, INDEX_R_ZARC_OFFSET + index]) * tf.pow(
+                    exp_frequencies,
+                    -processed_params[:,INDEX_PHI_WARBURG]
+                ) * tf.expand_dims(
+                    model_meta[:, MODEL_META_WARBURG_INCEPTION], axis=1)
 
-            warburg_impedance = tf.complex(
-                real_bad_piece * bad_piece2, imag_bad_piece * bad_piece2
-            )
+                warburg_impedance = tf.complex(
+                    real_bad_piece * bad_piece2, imag_bad_piece * bad_piece2
+                )
 
-            complex_one = tf.cast(tf.complex(1.,0.), dtype=tf.complex128)
-            #TODO: fix this error:
-            '''
-            
-
-                NotFoundError (see above for traceback): No registered 'Reciprocal' OpKernel for GPU devices compatible with node node truediv_4 (defined at EISFittingModelDefinitions.py:235)  = Reciprocal[T=DT_COMPLEX128, _device="/job:localhost/replica:0/task:0/device:GPU:0"](add_11)
-                 (OpKernel was found, but attributes didn't match)
-                .  Registered:  device='CPU'; T in [DT_FLOAT]
-                device='CPU'; T in [DT_HALF]
-                device='CPU'; T in [DT_DOUBLE]
-                device='CPU'; T in [DT_COMPLEX64]
-                device='CPU'; T in [DT_COMPLEX128]
-                device='GPU'; T in [DT_FLOAT]
-                device='GPU'; T in [DT_HALF]
-                device='GPU'; T in [DT_DOUBLE]
-                device='GPU'; T in [DT_INT64]
+                complex_one = tf.cast(tf.complex(1.,0.), dtype=tf.complex128)
+                #TODO: fix this error:
+                '''
                 
-                 [[node truediv_4 (defined at EISFittingModelDefinitions.py:235)  = Reciprocal[T=DT_COMPLEX128, _device="/job:localhost/replica:0/task:0/device:GPU:0"](add_11)]]
+    
+                    NotFoundError (see above for traceback): No registered 'Reciprocal' OpKernel for GPU devices compatible with node node truediv_4 (defined at EISFittingModelDefinitions.py:235)  = Reciprocal[T=DT_COMPLEX128, _device="/job:localhost/replica:0/task:0/device:GPU:0"](add_11)
+                     (OpKernel was found, but attributes didn't match)
+                    .  Registered:  device='CPU'; T in [DT_FLOAT]
+                    device='CPU'; T in [DT_HALF]
+                    device='CPU'; T in [DT_DOUBLE]
+                    device='CPU'; T in [DT_COMPLEX64]
+                    device='CPU'; T in [DT_COMPLEX128]
+                    device='GPU'; T in [DT_FLOAT]
+                    device='GPU'; T in [DT_HALF]
+                    device='GPU'; T in [DT_DOUBLE]
+                    device='GPU'; T in [DT_INT64]
+                    
+                     [[node truediv_4 (defined at EISFittingModelDefinitions.py:235)  = Reciprocal[T=DT_COMPLEX128, _device="/job:localhost/replica:0/task:0/device:GPU:0"](add_11)]]
+    
+                
+                '''
+                impedance += r / (
+                    (1./(1. + warburg_impedance)) +
+                        tf.pow((imag_freq / w_c), phi))
 
-            
-            '''
-            impedance += r / (
-                (1./(1. + warburg_impedance)) +
-                    tf.pow((imag_freq / w_c), phi))
+            else:
+                impedance += r / (1. + tf.pow((imag_freq / w_c), phi))
 
-        else:
-            impedance += r / (1. + tf.pow((imag_freq / w_c), phi))
+        impedance_real = tf.real(impedance)
+        impedance_imag = tf.imag(impedance)
 
-    impedance_real = tf.real(impedance)
-    impedance_imag = tf.imag(impedance)
+        impedance_stacked = tf.to_float(tf.stack([impedance_real, impedance_imag], axis=2))
 
-    impedance_stacked = tf.to_float(tf.stack([impedance_real, impedance_imag], axis=2))
-
-    return impedance_stacked
+        return impedance_stacked
 
 
 
--- MakeModels.bat
+++ MakeModels.bat
@@ -6,4 +6,9 @@ REM python EISFittingModelDefinitions_v2.py --mode=train --logdir=OnePercentTrai
 REM python EISFittingModelDefinitions_v2.py --mode=train --logdir=OnePercentTraining_wide --num_zarcs_training_lower=1 --inductances_training_lower=0 --num_conv=3 --conv_filters=40
 REM python EISFittingModelDefinitions_v2.py --mode=train --logdir=OnePercentTraining_deep --num_zarcs_training_lower=1 --inductances_training_lower=0 --num_conv=5 --conv_filters=20
 REM python EISFittingModelDefinitions_v2.py --mode=train --logdir=OnePercentTraining_big4 --num_zarcs_training_lower=1 --inductances_training_lower=0 --num_conv=5 --conv_filters=56
-python EISFittingModelDefinitions.py --mode=train --logdir=OnePercentTraining_june30 --num_zarcs_training_lower=1 --inductances_training_lower=0 --inception_training_lower=0 --num_conv=5 --conv_filters=40
+REM python EISFittingModelDefinitions.py --mode=train --logdir=OnePercentTraining_june30 --num_zarcs_training_lower=1 --inductances_training_lower=0 --inception_training_lower=0 --num_conv=5 --conv_filters=40
+
+python EISFittingModelDefinitions.py --mode=train --logdir=OnePercentTraining_july1 --num_zarcs_training_lower=1 --inductances_training_lower=0 --inception_training_lower=0 --num_conv=7 --conv_filters=40
+
+
+REM python EISFittingModelDefinitions.py --mode=train --logdir=OnePercentTraining_june30_regression_test1 --num_zarcs_training_lower=1 --inductances_training_lower=0 --inception_training_lower=0 --inception_training_upper=0 --num_conv=5 --conv_filters=40
