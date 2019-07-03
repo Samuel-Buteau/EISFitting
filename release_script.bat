da83642eda9779893f1bfb6f5cfc92d6863aa26d
Samuel Buteau <Samuel.Buteau@dal.ca>
2019-07-03 12:05:10 -0300

    reconfigured

--- release_script.bat
+++ release_script.bat
@@ -1,3 +1,6 @@
 del db.sqlite3
+del EIS\migrations\*
+copy NUL EIS\migrations\__init__.py
+python manage.py makemigrations
 python manage.py migrate
 python manage.py edit_database --mode=add_default_inverse_models
