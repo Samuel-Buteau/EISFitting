85370af7ae033a8d9fe4eadbf2a01b3cdce3527e
Samuel Buteau <Samuel.Buteau@dal.ca>
2019-07-24 09:54:29 -0300

    works on linux now, with bigger batches :)

---
+++ MakeModels.sh
@@ -0,0 +1,2 @@
+
+python3 EISFittingModelDefinitions.py --mode=train --logdir=OnePercentTraining_july2 --num_zarcs_training_lower=1 --inductances_training_lower=0 --inception_training_lower=0 --inception_training_upper=1 --num_conv=7 --conv_filters=40
